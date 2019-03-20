a7337c7a988cd7b2a7a122153713d56ba3b1b65e
Johan Røed <johan@rendra.io>
2019-03-20 17:27:15 +0100

    update dependencies

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,13 +8,10 @@ module.exports = {
       }
     },
     {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0',
         }
       }
     },
